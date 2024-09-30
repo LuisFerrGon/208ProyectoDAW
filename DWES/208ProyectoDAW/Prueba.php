b06cdeed65a8f27b84f2fc00c163d375db223aba
Luis Ferreras <luis.fergon.2@educa.jcyl.es>
2024-09-30 09:09:42 +0200

    V0.1

---
+++ DWES/208ProyectoDAW/Prueba.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--Autor: Luis Ferreras González-->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Index</title>
+        <link type="text/css" rel="stylesheet" href="webroot/estilos.css">
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+        </header>
+        <main>
+            <table>
+                <tr>
+                    <th span="col">Ejercicio</th>
+                    <th span="col">Enunciado</th>
+                </tr><!--Head-->
+                <tr>
+                    <td>0</td>
+                    <td>Hola mundo y phpinfo()</td>
+                    <td><a href="codigoPHP/ejercicio00.php">Ejecutar</a></td>
+                    <td><a href="mostrarcodigo/muestraEjercicio00.php">Mostrar</a></td>
+                </tr><!--00-->
+                <tr>
+                    <td>1</td>
+                    <td>Variables</td>
+                    <td><a href="codigoPHP/ejercicio01.php">Ejecutar</a></td>
+                    <td><a href="mostrarcodigo/muestraEjercicio01.php">Mostrar</a></td>
+                </tr><!--01-->
+                <tr>
+                    <td>2</td>
+                    <td>Variables</td>
+                    <td><a href="codigoPHP/ejercicio02.php">Ejecutar</a></td>
+                    <td><a href="mostrarcodigo/muestraEjercicio02.php">Mostrar</a></td>
+                </tr><!--02-->
+            </table>
+        </main>
+        <footer>
+            <a href="../index.php">Luis Ferreras</a>
+            <a href="../208DWESProyectoDWES/indexProyectoDWES.php">DWES</a>
+            <a href="https://github.com/LuisFerrGon/208DWESProyectoTema3">GitHub</a>
+        </footer>
+    </body>
+</html>
